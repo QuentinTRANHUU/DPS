af53d7957f6df2c0c31a1930cf8b9922be4048fd
FleauLePreux <quentin.tranhuu@hotmail.com>
2024-07-23 10:13:48 +0200

    ajout des derniers dataset clean

--- pages/Diabete.py
+++ pages/Diabete.py
@@ -4,7 +4,7 @@ import generique_page_maladie
 # Enregistrement de la page
 register_page(__name__)
 
-df_relative_path = r"\dataframes\clean_ckd_df.csv"
+df_relative_path = r"\dataframes\diabete_clean.csv"
 
 # Créez la mise en page
 layout = generique_page_maladie.create_layout(df_relative_path, "Diabète")
