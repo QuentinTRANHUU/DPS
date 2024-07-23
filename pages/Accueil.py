115b296ad30dbe89307a1874e5cf32734136497f
romain64200 <romain.caron64200@gmail.com>
2024-07-23 16:27:13 +0200

    Ajout texte image accueil

--- pages/Accueil.py
+++ pages/Accueil.py
@@ -30,7 +30,7 @@ layout = dbc.Container([
                                         [
                                             html.H4("Le monde va mal", className="card-title text-center p-4"),
                                             html.P(
-                                                "blabalbalbalaipoehfagçfdaohdfghblabalbalbalaipoehfagçfdaohdfghblabalbalbalaipoehfagçfdaohdfgh",
+                                                "Les maladies chroniques, telles que le diabète, les maladies cardiovasculaires,les maladies rénales et les maladies hépatiques, touchent des millions de personnes chaque année. Malgré les avancées médicales, trop de diagnostics sont posés tardivement, compromettant la qualité de vie et augmentant les coûts de traitement. Il est primordial d'inverser cette tendance préoccupante",
                                                 className="card-text",
                                             )
                                         ]
@@ -40,7 +40,7 @@ layout = dbc.Container([
                                 
                                 dbc.Col(
                                     dbc.CardImg(
-                                        src=r"\assets\images\doctor.jpg",
+                                        src=r"\assets\images\ACCUEIL_le_monde_va_mal.png",
                                         className="img-fluid rounded-start",
                                     ),
                                     className="col-md-4",
@@ -67,7 +67,7 @@ layout = dbc.Container([
                             [
                                 dbc.Col(
                                     dbc.CardImg(
-                                        src=r"\assets\images\doctor.jpg",
+                                        src=r"\assets\images\ACCUEIL_Qui_sommes_nous.png",
                                         className="img-fluid rounded-start",
                                     ),
                                     className="col-md-4",
@@ -77,7 +77,7 @@ layout = dbc.Container([
                                         [
                                             html.H4("Qui sommes-nous ?", className="card-title text-center p-4"),
                                             html.P(
-                                                "blabalbalbalaipoehfagçfdaohdfghblabalbalbalaipoehfagçfdaohdfghblabalbalbalaipoehfagçfdaohdfgh",
+                                                "Nous sommes une équipe de professionnels de la data unis par une mission commune : transformer la prévention des maladies chroniques. Nous avons développé des outils de dépistage avancés qui permettent de détecter les signes avant-coureurs de maladies avant qu'elles ne deviennent critiques. Notre objectif est d'offrir à chacun la possibilité de prendre en main sa santé grâce à des solutions fiables et accessibles",
                                                 className="card-text",
                                             )
                                         ]
@@ -109,9 +109,22 @@ layout = dbc.Container([
                                         [
                                             html.H4("Nos valeurs", className="card-title text-center p-4"),
                                             html.P(
-                                                "blabalbalbalaipoehfagçfdaohdfghblabalbalbalaipoehfagçfdaohdfghblabalbalbalaipoehfagçfdaohdfgh",
-                                                className="card-text",
-                                            )
+                                                "Précision : Chaque diagnostic est basé sur des données cliniques rigoureusement validées par des experts.",
+                                                className="card-text"
+                                                ),
+                                            html.P(
+                                                "Accessibilité : Nous croyons que chaque individu mérite un accès égal aux outils de dépistage de pointe, indépendamment de sa situation géographique ou socio-économique."
+                                                ,className="card-text"
+                                                ),
+                                            html.P(
+                                                "Innovation : Nous sommes constamment à la recherche de nouvelles méthodes pour améliorer nos technologies et offrir des solutions toujours plus efficaces."
+                                                ,className="card-text"
+                                                ),
+                                            html.P(
+                                                "Compassion : Nous plaçons l'humain au cœur de notre démarche, en prenant soin de chaque patient avec respect et empathie."
+                                                ,className="card-text"
+                                                )
+
                                         ]
                                     ),
                                     className="col-md-8",
@@ -119,7 +132,7 @@ layout = dbc.Container([
                                 
                                 dbc.Col(
                                     dbc.CardImg(
-                                        src=r"\assets\images\doctor.jpg",
+                                        src=r"\assets\images\ACCUEIL_Nos_valeurs.png",
                                         className="img-fluid rounded-start",
                                     ),
                                     className="col-md-4",
@@ -146,7 +159,7 @@ layout = dbc.Container([
                             [
                                 dbc.Col(
                                     dbc.CardImg(
-                                        src=r"\assets\images\doctor.jpg",
+                                        src=r"\assets\images\ACCUEIL_Notre_solution.png",
                                         className="img-fluid rounded-start",
                                     ),
                                     className="col-md-4",
@@ -156,7 +169,7 @@ layout = dbc.Container([
                                         [
                                             html.H4("Notre Solution", className="card-title text-center p-4"),
                                             html.P(
-                                                "blabalbalbalaipoehfagçfdaohdfghblabalbalbalaipoehfagçfdaohdfghblabalbalbalaipoehfagçfdaohdfgh",
+                                                "Notre solution repose sur un modèle de prédiction avancé, conçu pour identifier les risques de maladies chroniques avec une précision inégalée. En combinant des algorithmes d'intelligence artificielle avec des données cliniques actualisées, nous offrons un dépistage personnalisé qui tient compte de l'historique médical, des habitudes de vie, et des facteurs génétiques de chaque individu. Grâce à notre plateforme intuitive, vous pouvez réaliser un dépistage complet en quelques clics et recevoir des recommandations personnalisées pour prévenir les complications. Ensemble, transformons la prévention et faisons de la santé une priorité accessible à tous",
                                                 className="card-text",
                                             )
                                         ]
